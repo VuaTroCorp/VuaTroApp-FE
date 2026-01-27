63f1f5d6eb0c4489c5f0db08152778aa8e085fcb
vuatrovnapp <appvuatrovn@gmail.com>
2026-01-27 15:37:28 +0700

    [TriNT27][Update]: Merge develop into main
    * VTVN-31: first commit
    
    * VTVN-31: restructure project folders
    
    * VTVN-31: add header UI
    
    * VTVN-31: complete HomeFE
    
    * setting up ci/cd
    
    * test failed
    
    * revert
    
    * test failed
    
    * update package-lock.json
    
    * hh
    
    * test failed
    
    * bruh
    
    * correct them
    
    * test failed
    
    * [TriNT27][Provided]: setting ci/cd workflow
    
    * update ci name
    
    * correct test suite
    
    * a
    
    * change ci name
    
    ---------
    
    Co-authored-by: TruongVoXuan <truong.75.cvan@gmail.com>

---
+++ src/shared/components/header/Header.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+jest.mock("assets/images/logo.png", () => "logo.png");
+jest.mock("assets/icons/location1.png", () => "location1.png");
+jest.mock("assets/icons/down.png", () => "down.png");
+jest.mock("assets/icons/heart.png", () => "heart.png");
+jest.mock("assets/icons/account.png", () => "account.png");
+
+describe("Header Component", () => {
+  test("renders header component correctly", () => {
+    render(<Header />);
+
+    expect(screen.getByAltText("VuaTro")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Tìm Bất Động Sản...")).toBeInTheDocument();
+    expect(screen.getByText("Đăng nhập")).toBeInTheDocument();
+    expect(screen.getByText("Đăng tin")).toBeInTheDocument();
+  });
+});
