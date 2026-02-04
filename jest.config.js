2f879f17f738ff7858ac5c20ce73e2b35a7f7801
LePhucThanh-2004 <thanh.lp.64cntt@ntu.edu.vn>
2026-02-04 14:24:49 +0700

    update:

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-export default {
+module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  "jest": {
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  }
-}
 };
