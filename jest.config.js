26ff94d4bacfad6f4e583753621b958699aec8f0
LePhucThanh-2004 <thanh.lp.64cntt@ntu.edu.vn>
2026-02-04 14:09:02 +0700

    mo ta thay doi

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-
-export const moduleNameMapper = {
-  '^assets/(.*)$': '<rootDir>/src/assets/$1',
-  "\\.(png|jpg|jpeg|svg)$": "<rootDir>/__mocks__/fileMock.js"
-  // add any other aliases as needed
+export default {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
