8eaeea80e0d49bb308585359b52a9a1b6d27384a
LePhucThanh-2004 <thanh.lp.64cntt@ntu.edu.vn>
2026-02-04 13:29:09 +0700

    mo ta thay doi

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,9 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
+// jest.config.js
+module.exports = {
+  moduleNameMapper: {
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+  },
+};
