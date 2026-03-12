af49e5054bc589476832e600807dd73f1af96e35
Nguyễn Quang Trí Lê <153410920+Tig0nn@users.noreply.github.com>
2026-03-12 14:57:32 +0700

    [TriLNQ1@fpt.com][update]: integrate room search UI + API
    * fix: merge
    
    * fix:merge
    
    * apply api searchForRoom & mock data room

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ import RegisterPage from "pages/Auth/RegisterPage/RegisterPage";
 import ForgotPasswordPage from "pages/Auth/ForgotPasswordPage/ForgotPasswordPage";
 // import VerifyOtpPage from "pages/Auth/VerifyOtpPage/VerifyOtpPage";
 import ResetPasswordPage from "pages/Auth/ResetPasswordPage/ResetPasswordPage";
+import GoogleCallback from "pages/Auth/GoogleCallback/GoogleCallback";
 import HomePage from "pages/User/Home/HomePage";
 //FEATURES
 import PostNewsPage from "pages/User/PostNews/PostNewsPage";
@@ -29,9 +30,6 @@ export default function AppRoutes() {
         </Route>
       </Route>
 
-
-
-
       <Route element={<UserLayout />}>
         <Route index element={<HomePage />} />
         <Route path="user" element={<RoleBasedRoute allowedRoles={["USER"]} />}>
@@ -44,9 +42,6 @@ export default function AppRoutes() {
         />
         <Route path="/" element={<HomePage />} />
       </Route>
-
-
-
 
       <Route element={<AdminLayout />}>
         {/* <Route index element={<HomePage/>}/> */}
